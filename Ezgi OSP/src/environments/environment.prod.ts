f8cdae0cd282b5085f06558f55220f0872bd1d2a
EzgiErdoğan <erdogan75ezgi@gmail.com>
2023-01-12 21:36:00 +0300

    onlinesinavportali

---
+++ Ezgi OSP/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'ezgiangular',
+    appId: '1:784955883454:web:fb276542ddcaf5edf303ef',
+    storageBucket: 'ezgiangular.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD-oEVpAPBn-AWUyXcUb5QacdWO__OD4qM',
+    authDomain: 'ezgiangular.firebaseapp.com',
+    messagingSenderId: '784955883454',
+    measurementId: 'G-YVPN9JW6MZ',
+  },
+  production: true
+};
---
+++ Ezgi OSP/src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'ezgiangular',
+    appId: '1:784955883454:web:fb276542ddcaf5edf303ef',
+    storageBucket: 'ezgiangular.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyD-oEVpAPBn-AWUyXcUb5QacdWO__OD4qM',
+    authDomain: 'ezgiangular.firebaseapp.com',
+    messagingSenderId: '784955883454',
+    measurementId: 'G-YVPN9JW6MZ',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
